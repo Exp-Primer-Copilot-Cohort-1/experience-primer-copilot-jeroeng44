ecb0ab696dad55d51e4c07919301b0ec7dc075fb
jeroeng44 <126883680+jeroeng44@users.noreply.github.com>
2024-06-14 11:59:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,59 +2,24 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var path = require('path');
-var qs = require('querystring');
-var comments = [];
+var querystring = require('querystring');
 
-var server = http.createServer(function (req, res) {
-    //parse url
-    var urlObj = url.parse(req.url, true);
-    //get path
-    var pathName = urlObj.pathname;
-    //get query
-    var query = urlObj.query;
-    //get method
-    var method = req.method;
+var server = http.createServer(function(req);
+{
+    var url_parts = url.parse(req.url);
+    var path = url_parts.pathname;
+    var query = url_parts.query;
 
-    //router
-    if (pathName === '/') {
-        fs.readFile('./index.html', function (err, data) {
-            if (err) {
-                throw err;
-            }
-            res.end(data);
-        });
-    } else if (pathName === '/comment') {
-        //get comments
-        if (method === 'GET') {
-            var jsonStr = JSON.stringify(comments);
-            res.end(jsonStr);
-        }
-        //add comments
-        else if (method === 'POST') {
-            var str = '';
-            req.on('data', function (chunk) {
-                str += chunk;
-            });
-            req.on('end', function () {
-                var comment = qs.parse(str);
-                comments.push(comment);
-                var jsonStr = JSON.stringify(comments);
-                res.end(jsonStr);
-            });
-        }
-    } else {
-        var filePath = path.join(__dirname, pathName);
-        fs.readFile(filePath, function (err, data) {
-            if (err) {
-                res.statusCode = 404;
-                res.end('Not Found');
-            }
-            res.end(data);
-        });
+    switch(path)
+    {
+        case '/':
+            display_form(req, res);
+            break;
+        case '/comments':
+            save_comment(req, res);
+            break;
+        default:
+            display_404(req, res);
+            break;
     }
-});
-
-server.listen(3000, function () {
-    console.log('Server is running...');
-});
+}   
