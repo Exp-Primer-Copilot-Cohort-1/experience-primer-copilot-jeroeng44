c630281c62d96079766611d77dcad749baf14c66
jeroeng44 <126883680+jeroeng44@users.noreply.github.com>
2024-06-14 12:02:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,3 @@
 //create web server
 var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-
-var server = http.createServer(function(req);
-{
-    var url_parts = url.parse(req.url);
-    var path = url_parts.pathname;
-    var query = url_parts.query;
-
-    switch(path)
-    {
-        case '/':
-            display_form(req, res);
-            break;
-        case '/comments':
-            save_comment(req, res);
-            break;
-        default:
-            display_404(req, res);
-            break;
-    }
-}   
+var fs = require(
